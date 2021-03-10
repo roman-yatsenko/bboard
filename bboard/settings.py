9ea4ba7b726f71260388bd82671f5c93f05f9bca
YaRo <roman.yatsenko@hneu.net>
2021-03-10 16:59:18 +0200

    Add base template

--- bboard/settings.py
+++ bboard/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig'
+    'main.apps.MainConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
